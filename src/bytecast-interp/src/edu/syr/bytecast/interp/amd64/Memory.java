1ce68789a7211ba384d9af9d3153cee5dc47bcf7
Shawn Mueller <smueller@syr.edu>
2013-03-30 12:59:12 -0400

    Introduced the Memory to the interpreter

---
+++ src/bytecast-interp/src/edu/syr/bytecast/interp/amd64/Memory.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of Bytecast.
+ *
+ * Bytecast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bytecast is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bytecast.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package edu.syr.bytecast.interp.amd64;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class Memory {
+    Map<Long, Byte> m_memory;
+    
+    Memory(){
+        m_memory = new TreeMap<Long,Byte>();
+    }
+    
+    //Stores a value of num_bytes in a little endian format
+    public void setValue(long address, long value, long num_bytes){
+        long mask = 0x00000000000000ff;
+        for(int i = 0; i < num_bytes; i++)
+        {
+            byte byte_val = (byte)((value & (mask << 8*i)) >> 8*i); 
+            m_memory.put(address+i, byte_val);
+        }
+    }
+    
+    //Retrieves a value from memory
+    public long getValue(long address, long num_bytes){
+        long ret=0;
+        long mask = 0x00000000000000ffL;
+        for(int i = 0; i < num_bytes; i++){
+            Byte byte_val = m_memory.get(address+i);
+            long shifted_val = (mask & byte_val) << (i*8);
+            ret |= shifted_val;
+        }        
+        
+        return ret;
+    }        
+    
+    public static void main(String args[]){
+        Memory test = new Memory();
+        test.setValue(11, 0x123456789ABCL, 6);
+        System.out.println(Long.toHexString(test.getValue(11, 6)));
+        test.setValue(32, 0xFDCBA9876543210L, 8);
+        System.out.println(Long.toHexString(test.getValue(32, 8)));
+    }
+}
