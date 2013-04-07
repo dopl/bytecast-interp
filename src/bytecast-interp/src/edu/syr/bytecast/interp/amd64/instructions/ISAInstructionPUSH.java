a5ed1e103bbd488b71c82d60210c7aa49e1ed61d
Phil Pratt-Szeliga <pcpratts@trifort.org>
2013-04-07 13:03:19 -0400

    adding interfaces to pop and push instructions

--- src/bytecast-interp/src/edu/syr/bytecast/interp/amd64/instructions/ISAInstructionPUSH.java
+++ src/bytecast-interp/src/edu/syr/bytecast/interp/amd64/instructions/ISAInstructionPUSH.java
@@ -18,7 +18,22 @@
 
 package edu.syr.bytecast.interp.amd64.instructions;
 
+import edu.syr.bytecast.amd64.api.constants.InstructionType;
+import edu.syr.bytecast.amd64.api.instruction.IInstruction;
+import edu.syr.bytecast.interp.amd64.AMD64Environment;
+import edu.syr.bytecast.interp.amd64.IISAInstruction;
 
-public class ISAInstructionPUSH {
+
+public class ISAInstructionPUSH implements IISAInstruction {
+
+  @Override
+  public InstructionType getInstructionType() {
+      return InstructionType.PUSH;
+  }
+
+  @Override
+  public long execute(AMD64Environment env, IInstruction instruction) {
+      return 0;
+  }
 
 }
